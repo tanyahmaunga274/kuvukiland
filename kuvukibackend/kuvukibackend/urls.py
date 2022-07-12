369b2a039365a413df4c662729c3781481018a00
Emmanuel Maunga <tanyahmaunga274@gmail.com>
2022-07-12 12:10:21 +0200

    Change the authentication system to JWT authentication

--- kuvukibackend/kuvukibackend/urls.py
+++ kuvukibackend/kuvukibackend/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),
     path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.authtoken'))
+    path('api/auth/', include('djoser.urls.jwt'))
 ]
